b7ac2d13780ff338ef48332bff64b77a5536d45a
Alex Braatz <alexjbraatz@gmail.com>
2020-05-12 19:47:33 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 def turn(board)
-    puts "Please enter 1-9: "
+    puts "Please enter 1-9:"
     input = gets.chomp
     index = input_to_index(input)
     if valid_move?(board, index) == true
